0107bfc33c567ed71e0b74e63f1397eb858ce4bd
jiaqiLv <1031903858@qq.com>
2024-03-25 11:40:11 +0000

    chore(script):main_linear_iterative.sh

---
+++ FML/sample-code-UTD/FML/scripts/main_linear_iterative.sh
@@ -0,0 +1,9 @@
+python3 main_linear_iterative.py \
+    --batch_size 16 \
+    --label_rate 5 \
+    --learning_rate 0.001 \
+    --guide_flag 1 \
+    --method iterative \
+    --ckpt /code/MMFederated/FML/sample-code-UTD/FML/model/2024-03-25-06-30-54 \
+    --model_type global \
+    
