9653ca53013518f3f4f7455bfa746f7ed57e5a56
jiaqiLv <1031903858@qq.com>
2024-04-08 02:54:32 +0000

    feat(result_analysis):support to draw different indicator images

--- FML/sample-code-UTD/FML/result_analysis.py
+++ FML/sample-code-UTD/FML/result_analysis.py
@@ -1,19 +1,27 @@
 import matplotlib.pyplot as plt
 import os
 import re
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--type',type=str,default='f1_score',
+                    help='f1_score|acc')
+
+
 
 RESULT_FILES = [
     '/code/MMFederated/FML/sample-code-UTD/FML/model/2024-03-19-09-33-31/global.txt',
     '/code/MMFederated/FML/sample-code-UTD/FML/model/2024-03-22-03-31-35/client_0/client_0.txt',
     '/code/MMFederated/FML/sample-code-UTD/FML/model/2024-03-22-03-31-35/client_1/client_1.txt',
     '/code/MMFederated/FML/sample-code-UTD/FML/model/2024-03-22-03-31-35/client_2/client_2.txt',
+    '/code/MMFederated/FML/sample-code-UTD/FML/model/2024-03-25-03-13-46/global_tune.txt'
 ]
 
-
-
 if __name__ == '__main__':
+    args = parser.parse_args()
     acc_dict = dict()
     f1_score_dict = dict()
+    
     for result_file in RESULT_FILES:
         acc_list = []
         f1_score_list = []
@@ -26,14 +34,19 @@ if __name__ == '__main__':
                 f1_score_list.append(f1_score)
         acc_dict[ os.path.basename(result_file).split('.')[0]] = acc_list
         f1_score_dict[ os.path.basename(result_file).split('.')[0]] = f1_score_list
-    
-    for key, values in f1_score_dict.items():
-        print(key,max(values))
-        plt.plot(values, label=key)
+
+    if args.type == 'f1_score':
+        for key, values in f1_score_dict.items():
+            print(key,max(values))
+            plt.plot(values, label=key)
+    elif args.type == 'acc':
+        for key, values in acc_dict.items():
+            print(key,max(values))
+            plt.plot(values, label=key)
     # 添加图例和标签
     plt.legend()
     plt.xlabel('epochs')
-    plt.ylabel('acc')
+    plt.ylabel(f'{args.type}')
     # 展示图形
     plt.show()
-    plt.savefig('pictures/f1_score.png')
+    plt.savefig(f'pictures/{args.type}.png')
